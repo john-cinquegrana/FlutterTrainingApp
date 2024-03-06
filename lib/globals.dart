2498646647d50dfdd93f52804954026f6de28302
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-03-06 11:16:11 -0500

    Re-made the base starting UI

---
+++ lib/globals.dart
@@ -0,0 +1,7 @@
+/// A class that holds all of the global variables for the application. This
+/// contains information like the title of the app, authors, descriptions, or
+/// other primitives that should remain constant through the app.
+abstract class AppInfo {
+  /// The title of this application.
+  static const String title = 'Mood Swings';
+}
